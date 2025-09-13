d80b4197e1f0b70dca85e488ff3c5e5b6371ec9c
rikimario <mario_martinov@abv.bg>
2025-09-14 01:13:46 +0300

    can place flowers

---
+++ LeetCode/CanPlaceFlowers.js
@@ -0,0 +1,28 @@
+function canPlaceFlowers(flowerbed, n) {
+  if (n === 0) {
+    return true;
+  }
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0) {
+      let emptyLeft = i === 0 || flowerbed[i - 1] === 0;
+      let emptyRight = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+
+      if (emptyLeft && emptyRight) {
+        flowerbed[i] = 1;
+        n--;
+
+        if (n === 0) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1)); // turue
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2)); // false
+console.log(canPlaceFlowers([1, 0, 1, 0, 1, 0, 1], 1)); // false
+console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1)); // true?
