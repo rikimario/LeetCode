e0de16e16c0fc7e09ba0a4bddc98e6c13e22dc37
rikimario <mario_martinov@abv.bg>
2025-10-03 19:29:55 +0300

    reverse words

---
+++ LeetCode/two-pointers/ReverseWords.js
@@ -0,0 +1,18 @@
+function reverseWords(s) {
+  let chars = [...s];
+  let start = 0;
+
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] === " " || i === chars.length - 1) {
+      let end = i === chars.length - 1 && chars[i] !== " " ? i + 1 : i;
+      while (start < end) {
+        [chars[start], chars[end - 1]] = [chars[end - 1], chars[start]];
+        start++;
+        end--;
+      }
+      start = i + 1;
+    }
+  }
+}
+
+console.log(reverseWords("Let's take LeetCode contest"));
